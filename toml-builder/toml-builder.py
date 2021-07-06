98567efd9ce9ec14a0c2d8c84f6c6c5dfe0baaa8
Wiley <wileycorning@gmail.com>
2021-07-06 15:08:30 -0500

    Fix BVNeg not correctly converted; add array tests

--- toml-builder/toml-builder.py
+++ toml-builder/toml-builder.py
@@ -1,46 +1,27 @@
+import os,glob
 from jinja2 import Environment, FileSystemLoader
 
-env = Environment(
-    loader = FileSystemLoader(".")
-)
+prefix = "../messy/imperative/"
 
-template = env.get_template("regex-template.txt")
+allFiles = [fp.replace("\\","/")[len(prefix):] for fp in glob.glob(prefix+"**/*.sem",recursive=True)]
 
-def formulate(input):
-    charmap = dict()
-    
-    examples = []
-    
-    for key in input:
-        encoded = []
-        for c in key:
-            if not c in charmap.keys():
-                charmap[c] = len(charmap)+1
-            encoded.append(charmap[c])
-        examples.append({"i":encoded, "o": input[key]})
-    
-    return {"s": "s", "X": "X", "A": "A", "B": "B", "NA": 0,
-        "n": max([len(e["i"]) for e in examples]),
-        "charset": list(charmap.values()),
-        "examples": examples,
-    }
-    
-input = {
-    "": True,
-    "a": True,
-    "b": True,
-    "c": False,
-    "ab": True,
-    "ca": False,
-    "ba": True,
-    "ac": False,
-}
 
-txt = template.render(formulate(input)).strip()
+batches = dict()
+
+for batch,file in [(s[:s.index("/")],s) for s in allFiles]:
+    batches.setdefault(batch,[]).append(file)
+
+env = Environment(loader = FileSystemLoader("."))
+template = env.get_template("toml-template.txt")
+
+# all
+txt = template.render({"batches": batches}).strip()
 
 print(txt)
 
-with open('output.txt', 'w') as f:
+with open(prefix+'messy-imperative-all.toml', 'w') as f:
     f.write(txt)
-    
-print(formulate({"test":True, "best": False}))
+
+for batch in batches:
+    with open(prefix+batch+".toml", 'w') as f:
+        f.write(template.render({"batches": {batch: batches[batch]}}))
